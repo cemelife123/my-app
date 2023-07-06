74a2e399790182681b40ed7e38d688d0f0cbc8c4
cemelife123 <cemelife@nmsu.edu>
2023-07-06 11:52:24 -0600

    message

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,73 @@
 <template>
     <div id="app">
-      <ChannelList />
-      <ChatChannel/>
-      <ChatRoomList />
-      <UserList />
-      <DirectMessageList />
-      <ChatRoom />
-      <DirectMessage />
-      <MessageInput />
+      <div class="sidebar">
+        <ChannelList />
+        <ChatChannel/>
+        <ChatRoomList />
+        <UserList />
+        <DirectMessageList />
+      </div>
+      <div class="main">
+        <ChatRoom />
+        <DirectMessage />
+      </div>
+      <div class="input">
+        <MessageInput />
+      </div>
     </div>
   </template>
   
   <script>
-  // Import the DirectMessageList component
+  import ChatRoom from './components/ChatRoom.vue'
+  import MessageInput from './components/MessageInput.vue'
+  import ChatRoomList from './components/ChatRoomList.vue'
+  import UserList from './components/UserList.vue'
+  import ChatChannel from './components/ChatChannel.vue'
+  import ChannelList from './components/ChannelList.vue'
+  import DirectMessage from './components/DirectMessage.vue'
   import DirectMessageList from './components/DirectMessageList.vue'
-  // Other imports...
   
   export default {
     name: 'App',
     components: {
-      // Other components...
+      ChatRoom,
+      MessageInput,
+      ChatRoomList,
+      UserList,
+      ChatChannel,
+      ChannelList,
+      DirectMessage,
       DirectMessageList
     }
   }
   </script>
   
   <style>
-  /* Your styles... */
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    height: 100vh;
+  }
+  
+  .sidebar {
+    width: 20%;
+    background-color: #2c2f33;
+    color: #fff;
+  }
+  
+  .main {
+    width: 60%;
+    background-color: #36393f;
+    color: #dcddde;
+  }
+  
+  .input {
+    width: 20%;
+    background-color: #2c2f33;
+    color: #fff;
+  }
   </style>
   
+
